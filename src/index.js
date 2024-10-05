66738324f6106503fe23a7ce4c1c9b02b234bc3e
Bonolo Lekalakala <mmabosigo058@gmail.com>
2024-10-05 22:44:31 +0200

    Added API intergration

--- src/index.js
+++ src/index.js
@@ -1,10 +1,29 @@
-function searchCity(event){
-    event.preventDefault();
-    let searchInput = document.querySelector("#search-form-input");
-    let cityElement = document.querySelector("#city");
-    cityElement.innerHTML = searchInput.value;
-    
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+
+}
+
+function searchCity(city) {
+  let apiKey = "3449at19dbbcd0d32d0f4830249o0bf7";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+  axios.get(apiUrl).then(refreshWeather);
+}
+
+function searchEvent(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-form-input");
+  
+
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
-searchFormElement.addEventListener("submit", searchCity);
+searchFormElement.addEventListener("submit", searchEvent);
+
+searchCity("Paris");
+
